f498f6a7f21bb900d20badf62c2120f7296c7cb2
Chanuth <it20074418@my.sliit.lk>
2023-12-18 23:10:58 +0530

    add room listing

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ExistingRooms from "./components/room/ExistingRooms";
 import EditRoom from "./components/room/EditRoom";
 import Footer from "./components/layout/Footer";
 import NavBar from "./components/layout/NavBar";
+import RoomListing from "./components/room/RoomListing";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
             <Route path="/existing-rooms" element={<ExistingRooms />} />
             <Route path="/add-room" element={<AddRoom />} />
             <Route path="/edit-room/:roomId" element={<EditRoom />} />
+            <Route path="/browse-all-rooms" element={<RoomListing />} />
           </Routes>
         </Router>
         <Footer />
