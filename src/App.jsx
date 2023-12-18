c1ec5e5fd9f3d5dd795c1837e81bb102fa6aebe7
Chanuth <it20074418@my.sliit.lk>
2023-12-19 02:29:38 +0530

    design booking table

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,9 @@ import EditRoom from "./components/room/EditRoom";
 import Footer from "./components/layout/Footer";
 import NavBar from "./components/layout/NavBar";
 import RoomListing from "./components/room/RoomListing";
-import Admin from "./components/admin/Admin"
+import Admin from "./components/admin/Admin";
+import Checkout from "./components/booking/Checkout";
+import BookingSuccess from "./components/booking/BookingSuccess"
 
 function App() {
   return (
@@ -22,6 +24,8 @@ function App() {
             <Route path="/existing-rooms" element={<ExistingRooms />} />
             <Route path="/add-room" element={<AddRoom />} />
             <Route path="/edit-room/:roomId" element={<EditRoom />} />
+            <Route path="/book-room/:roomId" element={<Checkout />}/>
+            <Route path="/booking-success" element={<BookingSuccess />} />
             <Route path="/browse-all-rooms" element={<RoomListing />} />
             <Route path="/admin" element={<Admin />} />
           </Routes>
